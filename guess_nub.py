0e01d32def7d183a5665ebfe85f5091484db594c
lzb <lzb.coocof@gmail.com>
2016-03-26 22:47:19 +0800

    the final version

--- guess_nub.py
+++ guess_nub.py
@@ -3,19 +3,19 @@ import random
 secret = random.randint(1, 100)
 guess, tries = 0, 0
 
-for i in range(6):
+#for i in range(6):
+while tries < 6:
     print("pls guess the number:")
     guess = int(input())
-    tries += 1
     if guess == secret:
-        print("You are a lucky man! You guessed it!")
+        print("You are a lucky man! You guessed it! The number is %d" % secret)
+        print("You guessed %d times!" % (tries+1)) 
         break
     elif guess > secret:
         print("You guessed is too larger!")
     elif guess < secret:
         print ("you guessed is too small!")
-
-if tries < 6:
-    print("You guessed %d times!" % tries)
+    tries += 1
 else:
     print("You wasted too many times!")
+    print("The number is %d" % secret)
